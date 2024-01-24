949478194f2850104220564b2f9cdf5b33d3a710
mlpha9 <mlphat9@gmail.com>
2024-01-24 20:30:01 +0700

    Update agent_prompts

--- prompts/agent_prompts.py
+++ prompts/agent_prompts.py
@@ -2,7 +2,7 @@
 This file containts system prompts for various agents.
 """
 
-rag_user_proxy_prompt = """Manager_Assistant who has extra content retrieved
+rag_user_proxy_prompt = """RAG_User_Proxy who has extra content retrieved
 from given documents."""
 
 answer_generator_prompt = """Answer_Generator who generates accurate answer based on
@@ -10,7 +10,5 @@ retrieved content."""
 
 answer_evaluator_prompt = """Answer_Evaluator who evaluates the question's answer."""
 
-retrieve_assistant_prompt = """You are a helpful Retrieve_Assistant.
-As an Assistant, you specialize in retrieving the most relevant data from given documents.
-You find the information and then send the most relevant data
-to the RAG_proxy_agent for answer generation."""
+retrieve_assistant_prompt = """Retrieve_Assistant who specializes in retrieving
+the most relevant data from given documents."""
